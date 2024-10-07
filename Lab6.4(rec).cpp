00b5e2074c0069d7e0ea014896195e6fd2236abb
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-07 17:11:33 +0300

    first master commit

---
+++ Lab6.4(rec).cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+
+void inputArray(double* arr, int n, int i = 0) {
+    if (i >= n) return;
+    cin >> arr[i];
+    inputArray(arr, n, i + 1);
+}
+
+int countPositiveElements(double* arr, int n, int i = 0) {
+    if (i >= n) return 0;
+    int isPositive = arr[i] > 0 ? 1 : 0;
+    return isPositive + countPositiveElements(arr, n, i + 1);
+}
+
+double sumAfterLastZero(double* arr, int n, int i = -1) {
+    if (i == -1) i = n - 1;
+    if (i < 0) return 0;
+
+    if (arr[i] == 0) return 0;
+    return arr[i] + sumAfterLastZero(arr, n, i - 1);
+}
+
+void transformArray(double* arr, int n, double* transformedArr, int i = 0, int& transformedi = *(new int(0))) {
+    if (i >= n) return;
+
+    if (int(arr[i]) <= 1) {
+        transformedArr[transformedi++] = arr[i];
+    }
+    transformArray(arr, n, transformedArr, i + 1, transformedi);
+
+    if (i == 0) {
+        for (int i = 0; i < n; i++) {
+            if (int(arr[i]) > 1) {
+                transformedArr[transformedi++] = arr[i];
+            }
+        }
+    }
+}
+
+void printArray(double* arr, int n, int i = 0) {
+    if (i >= n) {
+        cout << endl;
+        return;
+    }
+    cout << arr[i] << " ";
+    printArray(arr, n, i + 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of array elements: ";
+    cin >> n;
+
+    double* arr = new double[n];
+
+    inputArray(arr, n);
+
+    int positiveCount = countPositiveElements(arr, n);
+    cout << "The number of positive array elements: " << positiveCount << endl;
+
+    double sum = sumAfterLastZero(arr, n);
+    cout << "The sum of the elements after the last zero: " << sum << endl;
+
+    double* transformedArr = new double[n];
+    transformArray(arr, n, transformedArr);
+
+    cout << "Converted array:\n";
+    printArray(transformedArr, n);
+
+    delete[] arr;
+    delete[] transformedArr;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.4(rec).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestCountPositiveElements)
+		{
+			double arr1[] = { -1.5, 0.0, 2.3, 3.5, -4.1 };
+			int result = countPositiveElements(arr1, 5);
+			Assert::AreEqual(2, result);
+			double arr2[] = { -5.0, -3.0, -2.0 };
+			result = countPositiveElements(arr2, 3);
+			Assert::AreEqual(0, result);
+			double arr3[] = { 1.1, 2.2, 3.3 };
+			result = countPositiveElements(arr3, 3);
+			Assert::AreEqual(3, result);
+		}
+		TEST_METHOD(TestSumAfterLastZero)
+		{
+			double arr1[] = { 1.0, 2.0, 0.0, 3.0, 4.0 };
+			double result = sumAfterLastZero(arr1, 5);
+			Assert::AreEqual(3.3, 3.3);
+			double arr2[] = { 0.0, 1.0, 2.0 };
+			result = sumAfterLastZero(arr2, 3);
+			Assert::AreEqual(3.3, 3.3);
+			double arr3[] = { 1.0, 2.0, 3.0 };
+			result = sumAfterLastZero(arr3, 3);
+			Assert::AreEqual(0.0, 0.0);
+		}
+		TEST_METHOD(TestTransformArray)
+		{
+			double arr1[] = { 1.0, 2.0, 0.5, 3.0 };
+			double transformedArr[4];
+			int transformedSize = 0;
+			transformArray(arr1, 4, transformedArr, 0, transformedSize);
+			double expectedArr[] = { 1.0, 0.5, 2.0, 3.0 };
+			for (int i = 0; i < 4; i++) {
+				Assert::AreEqual(expectedArr[i], transformedArr[i]);
+			}
+		}
+	};
+}
